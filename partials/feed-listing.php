8cc04877a4feb50551e63a8b3f45134f61f268c9
Michal Noskovic <michal@oneblackbear.com>
2019-07-02 12:14:56 +0100

    feed stuffs

--- partials/feed-listing.php
+++ partials/feed-listing.php
@@ -1,6 +1,7 @@
-<?php foreach($vacancies->vacancy_details as $key=>$vacancy) :?>
+<?php foreach($vacancies as $key=>$vacancy) :?>
     <?php if (!isset($homepage)) :?>
-    <div class="nhsuk-grid-row">
+        <div class="nhsuk-grid-row">
+        <?php //for homepage show 1st 3 ?>
     <?php elseif($key > 3) : break; endif; ?>
 
         <div class="<?php if (!isset($homepage)) :?>nhsuk-grid-column-one-third nhsuk-promo<?php else: ?>nhsuk-grid-column-one-quarter nhsuk-promo-group__item<?php endif; ?>" data-id="<?php echo $vacancy->id ?>">
@@ -11,22 +12,23 @@
                     <?php if (!isset($homepage)) :?>
                     <?php /* "job_employer", "job_closedate", "job_postdate", "job_staff_group"  */ ?>
                     <div class="nhsuk-summary-list">
-                        <?php foreach(["job_type", "job_salary", "job_location"] as $summary) :?>
+                        <?php foreach(["job_type", "job_salary", "job_location", "job_staff_group", "job_employer"] as $summary) :?>
                             <div class="nhsuk-summary-list__row">
                                 <dt class="nhsuk-summary-list__key"><?php echo ucfirst(str_replace(['job_', '_'], ['', ' '], $summary)); ?></dt>
                                 <dd class="nhsuk-summary-list__value"><?php echo $vacancy->{$summary}; ?></dd>
                             </div>
                         <?php endforeach; ?>
                     </div>
-                    <?php else: ?>
-                        <span class="nhsuk-button">Read more</span>
                     <?php endif; ?>
+
+                    <span class="nhsuk-button">Read more</span>
 
                     <!-- <p class="nhsuk-promo__description"><?php echo $vacancy->job_description ?></p> -->
                 </div>
             </a>
         </div>
+
     <?php if (!isset($homepage)) :?>
-    </div>
+        </div>
     <?php endif; ?>
 <?php endforeach; ?>
--- partials/feed.php
+++ partials/feed.php
@@ -1,32 +1,54 @@
 <?php
 include_once(get_stylesheet_directory() . '/functions/Feed.php');
 $feed = new Feed();
-$vacancies = $feed->importFeed();
+$feed_data = $feed->importFeed();
+$vacancies = $feed_data->vacancy_details;
+
+$filters = [
+    "job_type" => [
+        'title' => 'Contract Type',
+    ],
+    "job_staff_group" => [
+        'title' => 'Sector'
+    ],
+    "job_employer" => [
+        'title' => 'Employer'
+    ],
+];
+
+foreach($vacancies as $vacancy){
+    foreach($filters as $name => $filter) {
+        $filters[$name]['options'][$vacancy->$name] = true;
+    }
+}
 ?>
 <div class="nhsuk-grid-row">
     <div class="nhsuk-grid-column-one-third">
         <h3>Filter your results and find the role for you</h3>
         <form action="#">
+            <?php foreach($filters as $name => $filter): ?>
             <div class="nhsuk-form-group">
-                <fieldset class="nhsuk-fieldset" aria-describedby="example-disabled-hint">
-                    <legend class="nhsuk-fieldset__legend"><strong>Type</strong></legend>
-                    <span class="nhsuk-hint" id="example-disabled-hint">What type of contract are you looking for?</span>
-                    <div class="nhsuk-radios">
-                        <div class="nhsuk-radios__item">
-                            <input class="nhsuk-radios__input" id="type_1" name="type_0" type="radio" value="fixed">
-                            <label class="nhsuk-label nhsuk-radios__label" for="type_0">Fixed term</label>
+                <fieldset class="nhsuk-fieldset" aria-describedby="field_<?php echo $name; ?>">
+                    <legend class="nhsuk-fieldset__legend"><strong><?php echo $filter['title']?></strong></legend>
+                    <?php if(isset($filter['description']) && $filter['description']): ?>
+                    <span class="nhsuk-hint" id="field_<?php echo $name; ?>"><?php echo $filter['description']?></span>
+                    <?php endif; ?>
+                    <div class="nhsuk-checkboxes">
+                        <?php
+                            $count = 0;
+                            $options = array_keys($filter['options']);
+                            sort($options);
+                            foreach($options as $option):
+                        ?>
+                        <div class="nhsuk-checkboxes__item">
+                            <input class="nhsuk-checkboxes__input" id="<?php echo $name . '_' . $count; ?>" name="<?php echo $name; ?>" type="checkbox" value="<?php echo $option; ?>">
+                            <label class="nhsuk-label nhsuk-checkboxes__label" for="<?php echo $name . '_' . $count; ?>"><?php echo $option; ?></label>
                         </div>
-                        <div class="nhsuk-radios__item">
-                            <input class="nhsuk-radios__input" id="type_1" name="type_1" type="radio" value="permanent">
-                            <label class="nhsuk-label nhsuk-radios__label" for="type_1">Permanent</label>
-                        </div>
-                        <div class="nhsuk-radios__item">
-                            <input class="nhsuk-radios__input" id="type_2" name="type_2" type="radio" value="training">
-                            <label class="nhsuk-label nhsuk-radios__label" for="type_2">Training</label>
-                        </div>
+                        <?php $count++; endforeach; ?>
                     </div>
                 </fieldset>
             </div>
+            <?php endforeach; ?>
         </form>
     </div>
     <div class="nhsuk-grid-column-two-thirds">
