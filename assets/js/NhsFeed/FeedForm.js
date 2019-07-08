0c1bc4a791c234e8f3c5f0efe4e67c0c60094225
Michal Noskovic <michal@oneblackbear.com>
2019-07-08 23:04:26 +0100

    FeedForm.js is a class and filters employers

--- assets/js/NhsFeed/FeedForm.js
+++ assets/js/NhsFeed/FeedForm.js
@@ -1,40 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
-export default function FeedForm(props) {
-    const { filters, onFiltersChange } = props;
+export default class FeedForm extends Component {
 
-    return (
-        <form action="#">
-            {Object.entries(filters).map(([name, filter]) => (
+    constructor(props) {
+        super(props);
+        this.job_employer_search = React.createRef();
+
+        const { filters } = this.props;
+
+        this.state = {
+            filters: filters,
+        };
+
+        this.handleEmployerSearch = this.handleEmployerSearch.bind(this);
+    }
+
+    handleEmployerSearch(event) {
+        event.preventDefault();
+        const {filters} = this.props;
+        const job_employer_search = this.job_employer_search.current.value.toLowerCase();
+        let options = Object.entries(filters.job_employer.options).map(([name, option]) => {
+            option.show = name.toLowerCase().indexOf(job_employer_search) >= 0;
+            return {[name]:option};
+        });
+        filters.job_employer.options = Object.assign(...options);
+        this.setState({
+            filters: filters,
+        });
+    }
+
+    render() {
+        const { onFiltersChange } = this.props;
+
+        return (
+            <form action="#">
+            {Object.entries(this.state.filters).map(([name, filter]) => (
                 <div key={name}
                      className="nhsuk-form-group">
                     <fieldset className="nhsuk-fieldset">
                         <legend className="nhsuk-fieldset__legend">
                             <strong>{filter.title}</strong>
                         </legend>
+
+                        {name === 'job_employer' && (
+                            <span className="nhsuk-hint">
+                                <input type="text"
+                                       className="nhsuk-input"
+                                       placeholder="Search"
+                                       ref={this.job_employer_search}
+                                       onChange={this.handleEmployerSearch}
+                                />
+                            </span>
+                        )}
+
                         <div className="nhsuk-checkboxes">
                             {Object.entries(filter.options).map(([option, checked], key) => (
-                                <div key={name + key}
-                                     className="nhsuk-checkboxes__item">
-                                    <input className="nhsuk-checkboxes__input"
-                                           id={name + key}
-                                           name={name}
-                                           checked={checked}
-                                           onChange={() => onFiltersChange(name,option, !checked)}
-                                           type="checkbox"
-                                           value={option} />
-                                    <label className="nhsuk-label nhsuk-checkboxes__label"
-                                           htmlFor={name + key}
-                                    >{option}</label>
-                                </div>
+                                    <div key={name + key}
+                                         className={`nhsuk-checkboxes__item ${!checked.show && !checked.checked ? 'hidden' : checked.checked ? 'checked' : ''}
+                                    `}>
+                                        <input className="nhsuk-checkboxes__input"
+                                               id={name + key}
+                                               name={name}
+                                               checked={checked.checked}
+                                               onChange={() => onFiltersChange(name,option, !checked.checked)}
+                                               type="checkbox"
+                                               value={option} />
+                                        <label className="nhsuk-label nhsuk-checkboxes__label"
+                                               htmlFor={name + key}
+                                        >{option}</label>
+                                    </div>
                             ))}
                         </div>
                     </fieldset>
                 </div>
             ))}
-        </form>
-    );
+            </form>
+        );
+    }
 }
 
 FeedForm.propTypes = {
