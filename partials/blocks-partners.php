bd42422ce773a95149f17c61ad412713c4c8c8ac
Michal Noskovic <michal@oneblackbear.com>
2019-07-08 23:11:52 +0100

    partners link around image

--- partials/blocks-partners.php
+++ partials/blocks-partners.php
@@ -5,7 +5,7 @@
 ?>
 
     <div class="nhsuk-grid-column-one-quarter">
-        <a class="" href="<?php the_permalink(); ?>">
-            <?php the_post_thumbnail(); ?>
+        <a class="nhsuk-promo__link-wrapper" href="<?php the_permalink(); ?>">
+            <?php the_post_thumbnail(null, ['class' => 'nhsuk-promo__img wp-post-image']); ?>
         </a>
     </div>
