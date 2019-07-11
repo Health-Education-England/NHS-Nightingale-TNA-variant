6353f8f03649cd081f6d579d2ed803d6070b2ecd
Michal Noskovic <michal@oneblackbear.com>
2019-07-11 12:48:25 +0100

    link on partners

--- partials/blocks-partners.php
+++ partials/blocks-partners.php
@@ -2,10 +2,12 @@
 /**
  * Blocks Partners
  */
+
+$url = get_field('external_url');
 ?>
 
     <div class="nhsuk-grid-column-one-quarter">
-        <a class="nhsuk-promo__link-wrapper" href="<?php the_permalink(); ?>">
+        <?php if ($url) :?><a class="nhsuk-promo__link-wrapper" href="<?php echo $url; ?>"><?php endif; ?>
             <?php the_post_thumbnail(null, ['class' => 'nhsuk-promo__img wp-post-image']); ?>
-        </a>
+        <?php if ($url) :?></a><?php endif; ?>
     </div>
