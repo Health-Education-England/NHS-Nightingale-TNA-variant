3dff7334a89133aae6ab444da81b31152e0a8b3f
Michal Noskovic <michal@oneblackbear.com>
2019-07-11 16:03:39 +0100

    fix Single page padding on mobile

--- single.php
+++ single.php
@@ -11,36 +11,26 @@ get_header();
 ?>
 
     <div id="primary" class="nhsuk-width-container">
-        <main id="maincontent" class="nhsuk-main-wrapper">
-            <div class="nhsuk-grid-row">
-                <div class="nhsuk-grid__item nhsuk-grid-column-two-thirds">
-                    <div class="nhsuk-grid-row">
-                        <?php
-                        while ( have_posts() ) : the_post();
+        <main id="maincontent" class="nhsuk-main-wrapper nhsuk-grid-row">
+            <div class="nhsuk-grid__item nhsuk-grid-column-two-thirds">
+                    <?php
+                    while ( have_posts() ) : the_post();
 
-                            get_template_part( 'template-parts/content', get_post_type() );
+                        get_template_part( 'template-parts/content', get_post_type() );
 
-                            // If comments are open or we have at least one comment, load up the comment template.
+                        // If comments are open or we have at least one comment, load up the comment template.
 //                            if ( comments_open() || get_comments_number() ) :
 //                                comments_template();
 //                            endif;
 
-                        endwhile; // End of the loop.
-                        ?>
-                    </div>
-                </div>
-                <div class="nhsuk-grid__item nhsuk-grid-column-one-third">
-                    <div class="nhsuk-grid-row">
-                        <?php nightingale_2_0_post_thumbnail(); ?>
-                    </div>
-                </div>
+                    endwhile; // End of the loop.
+                    ?>
             </div>
-            <div class="nhsuk-grid-row">
-                <div class="nhsuk-grid__item nhsuk-grid-column-full">
-                    <div class="nhsuk-grid-row">
-                        <?php get_template_part('partials/pagination'); ?>
-                    </div>
-                </div>
+            <div class="nhsuk-grid__item nhsuk-grid-column-one-third">
+                <?php nightingale_2_0_post_thumbnail(); ?>
+            </div>
+            <div class="nhsuk-grid__item nhsuk-grid-column-full">
+                <?php get_template_part('partials/pagination'); ?>
             </div>
         </main><!-- #main -->
     </div><!-- #primary -->
