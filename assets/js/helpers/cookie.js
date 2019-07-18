a3ee110b3aacab5b079ea3754c22c1d0f00dc8d9
Michal Noskovic <michal@oneblackbear.com>
2019-07-18 14:21:05 +0100

    Cookie date to string fix

--- assets/js/helpers/cookie.js
+++ assets/js/helpers/cookie.js
@@ -6,7 +6,7 @@ const cookie = {
     setCookie(name, value, days) {
         let d = new Date;
         d.setTime(d.getTime() + 24*60*60*1000*days);
-        document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+        document.cookie = name + "=" + value + ";path=/;expires=" + d.toUTCString();
     },
     deleteCookie(name) { this.setCookie(name, '', -1); }
 };
