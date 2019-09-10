4a099d30270b8bd014a6dfb440cc57c890e991c0
Michal Noskovic <michal@oneblackbear.com>
2019-09-10 17:09:07 +0100

    Cookies are a plugin

--- functions/assets.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function nhsAssets() {
-    wp_enqueue_script( 'cookieNotice',  get_stylesheet_directory_uri() . '/assets/js/cookie.js', [], false, true );
-}
-add_action( 'wp_enqueue_scripts', 'nhsAssets', 100 );
