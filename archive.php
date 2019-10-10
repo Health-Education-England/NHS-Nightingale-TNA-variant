e9102ed696876b83549db5fae0988eea1b8352af
Michal Noskovic <michal@oneblackbear.com>
2019-10-10 15:52:57 +0100

    Faqs search form

--- archive.php
+++ archive.php
@@ -23,12 +23,31 @@ get_header();
                             <h1 class="page-title"><?php the_title(); ?></h1>
                             <div class="archive-description"><?php the_content(); ?></div>
                         </header><!-- .page-header -->
+
                         <?php
                         wp_reset_postdata();
                     }
                 ?>
 
                 <?php if ( have_posts() ) : ?>
+
+                    <?php if(get_post_type() == 'faqs'):?>
+                        <form method="get" action="" id="faqsearch" class="nhsuk-header__search-form">
+                            <div class="autocomplete-container" id="autocomplete-container">
+                                <div class="autocomplete__wrapper" role="combobox" aria-expanded="false">
+                                    <input type='search' placeholder="Search FAQs" class="search-field__listbox">
+                                </div>
+                            </div>
+                            <button class="nhsuk-search__submit" type="submit">
+                                <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                                    <path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path>
+                                </svg>
+                                <span class="nhsuk-u-visually-hidden">Search</span>
+                            </button>
+                        </form>
+                        <script src="/wp-content/themes/nhstna-theme/js/faqsearch.js"></script>
+                        <hr>
+                    <?php endif; ?>
 
                     <?php if(!$hasPage) : ?>
                     <header class="page-header">
@@ -50,11 +69,11 @@ get_header();
                          * called content-___.php (where ___ is the Post Type name) and that will be used instead.
                          */
                         ?>
-                    <div class="nhsuk-grid-row">
+                    <div class="nhsuk-grid-row <?php if(get_post_type() == 'faqs'):?>faqslisting<?php endif; ?>">
                         <?php
                         get_template_part( 'template-parts/content', get_post_type() );
                         ?>
-                    </div><hr/>
+                    </div><?php if(get_post_type() != 'faqs'):?><hr/><?php endif; ?>
                     <?php
                     endwhile;
 
