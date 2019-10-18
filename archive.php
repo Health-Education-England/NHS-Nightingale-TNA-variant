e3a0d724d8f082392a63be92a5004ed778b7cba7
Michal Noskovic <michal@oneblackbear.com>
2019-10-18 17:23:58 +0100

    fix faqs question ‘overhang’

--- archive.php
+++ archive.php
@@ -69,7 +69,7 @@ get_header();
                          * called content-___.php (where ___ is the Post Type name) and that will be used instead.
                          */
                         ?>
-                    <div class="nhsuk-grid-row <?php if(get_post_type() == 'faqs'):?>faqslisting<?php endif; ?>">
+                    <div class="<?php if(get_post_type() == 'faqs'):?>faqslisting<?php else:?>nhsuk-grid-row<?php endif; ?>">
                         <?php
                         get_template_part( 'template-parts/content', get_post_type() );
                         ?>
