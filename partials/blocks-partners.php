259b6dda23f7c3aa0f93a28afc7de7e700d4c645
Michal Noskovic <michal@oneblackbear.com>
2019-10-28 10:39:24 +0000

    fix partners block on mobile

--- partials/blocks-partners.php
+++ partials/blocks-partners.php
@@ -5,7 +5,7 @@
 
 $url = get_field('external_url');
 ?>
-<div class="nhsuk-grid-column-one-quarter">
+<div class="nhsuk-grid-column-one-quarter partners">
     <?php if ($url) :?><a class="nhsuk-promo__link-wrapper" href="<?php echo $url; ?>" target="_blank" aria-label="<?php echo the_title(); ?>"><?php endif; ?>
         <?php the_post_thumbnail(null, ['class' => 'nhsuk-promo__img wp-post-image']); ?>
     <?php if ($url) :?></a><?php endif; ?>
